1166cb1888f61d2e5a76f42838b3be0a0d1535a0
OSIsrael <israelos067@gmail.com>
2023-12-01 11:10:05 -0500

    Mandar datos de una pagina a otra con ROUTER
    Se usa Router para enviar y recibir datos desde la otra pagina.

--- src/app/tabs/inicio/inicio.page.ts
+++ src/app/tabs/inicio/inicio.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class InicioPage  {
+  numeroIngresado: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    this.numeroIngresado = 0;
   }
 
+  enviarNumero() {
+    if (!isNaN(this.numeroIngresado)) {
+      this.router.navigate(['/tabs/nudo', { numero: this.numeroIngresado }]);
+    }
+  }
 }
--- src/app/tabs/nudo/nudo.page.ts
+++ src/app/tabs/nudo/nudo.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-nudo',
   templateUrl: './nudo.page.html',
   styleUrls: ['./nudo.page.scss'],
 })
 export class NudoPage implements OnInit {
+  numeroRecibido: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+      this.numeroRecibido = 0;
+  }
 
   ngOnInit() {
+    const numeroParam = this.route.snapshot.params['numero'];
+    this.numeroRecibido = parseInt(numeroParam, 10);
   }
 
 }
